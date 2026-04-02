f8e81beefe1a6f2d6a8777eb343ebd31cef082ab
Hoangvu1520 <109757287+Hoangvu1520@users.noreply.github.com>
2026-04-02 19:23:24 +0700

    update

--- Assets/Scripts/UI/Loading/Initiate.cs
+++ Assets/Scripts/UI/Loading/Initiate.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Initiate : MonoBehaviour
+public static class Initiate
 {
-    // Start is called before the first frame update
-    void Start()
+    static bool areWeFading = false;
+
+    static GameObject initTemp = null;
+
+    //Create Fader object and assing the fade scripts and assign all the variables
+    public static void Fade(string scene, Color col, float multiplier)
     {
-        
+        if (areWeFading)
+        {
+            Debug.Log("Already Fading");
+            return;
+        }
+
+        GameObject init = new GameObject();
+        init.name = "Fader";
+        Canvas myCanvas = init.AddComponent<Canvas>();
+        myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        init.AddComponent<Fader>();
+        init.AddComponent<CanvasGroup>();
+        init.AddComponent<Image>();
+
+        Fader scr = init.GetComponent<Fader>();
+        scr.fadeDamp = multiplier;
+        scr.fadeScene = scene;
+        scr.fadeColor = col;
+        scr.start = true;
+        areWeFading = true;
+        scr.InitiateFader();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void FadeTemp(string scene, Color col, float multiplier)
     {
-        
+        if (areWeFading)
+        {
+            Debug.Log("Already Fading");
+            return;
+        }
+
+        if (initTemp == null)
+        {
+            initTemp = new GameObject();
+            initTemp.name = "Fader";
+            Canvas myCanvas = initTemp.AddComponent<Canvas>();
+            myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            initTemp.AddComponent<Fader>();
+            initTemp.AddComponent<CanvasGroup>();
+            initTemp.AddComponent<Image>();
+
+        }
+
+        Fader scr = initTemp.GetComponent<Fader>();
+        scr.fadeDamp = multiplier;
+        scr.fadeScene = scene;
+        scr.fadeColor = col;
+        scr.start = true;
+        areWeFading = true;
+        scr.InitiateFader();
+
+    }
+
+    public static void DoneFading()
+    {
+        areWeFading = false;
     }
 }
